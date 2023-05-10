79f1fb6ef00d29aa1e1533efb85c5333eeeff187
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-05-10 00:18:23 -0700

    Add sunrise and sunset

--- script.js
+++ script.js
@@ -18,6 +18,9 @@ let ipAddressText = document.getElementById("ipAddress");
 let weatherTempText = document.getElementById("weatherTemp");
 let weatherConditionsText = document.getElementById("weatherConditions");
 let weatherIcon = document.getElementById("weatherIcon");
+let visibilityText = document.getElementById("visibility");
+let sunriseText = document.getElementById("sunrise");
+let sunsetText = document.getElementById("sunset");
 
 
 async function getData() {
@@ -31,8 +34,7 @@ async function getData() {
     
         if (response.ok) {
             console.log("the IP API result is: " , result);
-
-            
+           
             let ipAdress = result.ip;
             let theCity = result.city;
             console.log(ipAdress);
@@ -64,13 +66,12 @@ function outputIpAddress(ipInput) {
 
 async function getWeatherData(ipAddressInput) {
 
+    let ipAddressValue = ipAddressInput;
+
     console.log("the value before calling weather API URL is: " + ipAddressInput);
 
      const weatherUrl = "https://api.weatherapi.com/v1/current.json?key=" + weatherAPIKey + "&q=" + ipAddressInput + "&aqi=no";
      
-     
-    // const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=" + weatherAPIKey + "&units=metric";
-
     try {
         const weatherResponse = await fetch(weatherUrl, {cache: "no-cache"});
         const weatherResult = await weatherResponse.json();
@@ -82,13 +83,16 @@ async function getWeatherData(ipAddressInput) {
             let theWeather = weatherResult.current.temp_c;
             let condtionDescription = weatherResult.current.condition["text"];
             let theWeatherIcon = weatherResult.current.condition["icon"];
+            let theVisibility = weatherResult.current.vis_km;
             
 
             outputWeatherTemp(theWeather);
             outputWeatherConditions(condtionDescription);
             outputWeatherIcon(theWeatherIcon);
-            
+            outputVisibility(theVisibility);
         }
+
+        getSunriseSunset(ipAddressValue);
 
     } catch (error) {
         if (error) throw error;
@@ -96,6 +100,41 @@ async function getWeatherData(ipAddressInput) {
     
     }
 }
+
+/**
+ * Get the Sunrise and Sunset from API using IP Address
+ * @param {*} ipAddressInput 
+ */
+
+async function getSunriseSunset(ipAddressInput) {
+
+    
+
+     const weatherUrl = "https://api.weatherapi.com/v1/astronomy.json?key=" + weatherAPIKey + "&q=" + ipAddressInput;
+     
+    try {
+        const weatherResponse = await fetch(weatherUrl, {cache: "no-cache"});
+        const weatherResult = await weatherResponse.json();
+
+
+        if (weatherResponse.ok) {
+            console.log("the Weather API Astronomy result is: " , weatherResult);
+
+            let theSunrise = weatherResult.astronomy.astro.sunrise;
+            let theSunset = weatherResult.astronomy.astro.sunset;
+
+            outputSunrise(theSunrise);
+            outputSunset(theSunset);
+            
+        }
+
+    } catch (error) {
+        if (error) throw error;
+        console.log("Weather API Astronomy error: ", error);
+    
+    }
+}
+
 
 
 function outputWeatherTemp(tempInput)
@@ -124,4 +163,17 @@ function outputWeatherIcon(iconInput)
     // weatherIconText.innerHTML = "<img src='http://openweathermap.org/img/w/" + theIconCode +  ".png'>";
 
 
+}
+
+function outputVisibility(visibilityInput) {
+    visibilityText.innerHTML = visibilityInput;
+}
+
+function outputSunset(sunsetInput) {
+
+    sunsetText.innerHTML = sunsetInput;
+}
+
+function outputSunrise(sunriseInput) {
+    sunriseText.innerHTML = sunriseInput;
 }
